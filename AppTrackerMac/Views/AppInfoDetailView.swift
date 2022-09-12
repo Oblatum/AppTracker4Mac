aa374d9cff2caa0a302b12422c662ed19a385f7a
Butanediol <n872485238@gmail.com>
2022-09-12 16:02:07 +0800

    Feat: drag & drop Cleanup: refactor detail view

--- AppTrackerMac/Views/AppInfoDetailView.swift
+++ AppTrackerMac/Views/AppInfoDetailView.swift
@@ -5,13 +5,18 @@
 //  Created by Butanediol on 2022/9/11.
 //
 
+import UniformTypeIdentifiers
 import SwiftUI
 
 struct AppInfoDetailView: View {
     
     var appInfo: AppInfoElement
     
+    @State var imageData: Data?
     @State var imageUrl: URL? = nil
+    @State var imageLoading: Bool = true
+    @State var showAlert: Bool = false
+    @State var viewError: Error? = nil
     @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
@@ -19,21 +24,32 @@ struct AppInfoDetailView: View {
             ZStack {
                 RoundedRectangle(cornerRadius: 12)
                     .foregroundColor(colorScheme == .light ? .white : .init(.displayP3, red: 0x1E/0xFF, green: 0x1E/0xFF, blue: 0x1E/0xFF))
-                AsyncImage(url: imageUrl) { image in
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-                        .padding()
-                        .contextMenu {
-                            Button("Save to...", action: saveIconTo)
-                                .keyboardShortcut("s", modifiers: .command)
-                            Button("Copy To Clipboard...", action: copyIcon)
-                                .keyboardShortcut("c", modifiers: .command)
-                        }
-                } placeholder: {
-                    ProgressView()
+                Group {
+                    if let imageData = imageData, let nsImage = NSImage(data: imageData) {
+                        Image(nsImage: nsImage)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                            .contextMenu {
+                                Button("Save to...", action: saveIcon)
+                                    .keyboardShortcut("s", modifiers: .command)
+                                Button("Copy to Clipboard", action: copyIcon)
+                                    .keyboardShortcut("c", modifiers: .command)
+                            }
+                    } else if imageLoading {
+                        ProgressView()
+                    } else {
+                        Image(systemName: "xmark.square.fill")
+                    }
                 }
+                .onDrag {
+                    if let imageData = imageData {
+                        return NSItemProvider(item: imageData as NSSecureCoding, typeIdentifier: UTType.png.identifier)
+                    } else {
+                        return NSItemProvider()
+                    }
+                }
+                .padding()
                 .frame(idealWidth: 200, maxWidth: 200, idealHeight:200, maxHeight: 200)
             }
             .frame(maxHeight: 216)
@@ -52,37 +68,39 @@ struct AppInfoDetailView: View {
             .padding()
             Spacer()
         }
-        .onAppear {
-            getImageUrl()
+        .task {
+            await getImage()
         }
-        .onChange(of: appInfo, perform: { newValue in
-            getImageUrl(packageName: newValue.packageName)
-        })
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text(viewError?.localizedDescription ?? "Unknown Error"))
+        }
+    }
+    
+    private func getImage() async {
+        let url = URL(string: "https://apptracker-api.cn2.tiers.top/api/icon?appId=\(appInfo.packageName)")!
+        do {
+            var (data, _) = try await URLSession.shared.data(from: url)
+            let imageUrlResponse = try JSONDecoder().decode(GetImageUrlResponse.self, from: data)
+            (data, _) = try await URLSession.shared.data(from: URL(string: imageUrlResponse.image)!)
+            imageData = data
+        } catch {
+            viewError = error
+        }
+        imageLoading = false
+    }
+    
+    private func saveIcon() {
+        guard let fileUrl = showSavePanel() else { return }
+        do {
+            try imageData?.write(to: fileUrl)
+        } catch {
+            viewError = error
+        }
     }
     
     private func copyIcon() {
-        guard let imageUrl = imageUrl else { return }
-        Task {
-            do {
-                let (imageData, _) = try await URLSession.shared.data(from: imageUrl)
-                NSPasteboard.general.clearContents()
-                NSPasteboard.general.setData(imageData, forType: .png)
-            } catch {
-                debugPrint(error.localizedDescription)
-            }
-        }
-    }
-    
-    private func saveIconTo() {
-        guard let imageUrl = imageUrl, let url = showSavePanel() else { return }
-        Task {
-            do {
-                let (imageData, _) = try await URLSession.shared.data(from: imageUrl)
-                try imageData.write(to: url)
-            } catch {
-                debugPrint(error.localizedDescription)
-            }
-        }
+        NSPasteboard.general.clearContents()
+        NSPasteboard.general.setData(imageData, forType: .png)
     }
     
     private func showSavePanel() -> URL? {
@@ -96,18 +114,6 @@ struct AppInfoDetailView: View {
     
         let response = savePanel.runModal()
         return response == .OK ? savePanel.url : nil
-    }
-    
-    private func getImageUrl(packageName: String? = nil) {
-        Task {
-            imageUrl = nil
-            let url = URL(string: "https://apptracker-api.cn2.tiers.top/api/icon?appId=\(packageName ?? appInfo.packageName)")!
-            do {
-                let (data, _) = try await URLSession.shared.data(from: url)
-                let imageUrlResponse = try JSONDecoder().decode(GetImageUrlResponse.self, from: data)
-                imageUrl = URL(string: imageUrlResponse.image)
-            } catch { debugPrint(error.localizedDescription) }
-        }
     }
 }
 
